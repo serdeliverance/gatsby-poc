e2adb0cadcd5b77a9c8752b782c39e4d2b014912
Sergio Cano <canosergio90@gmail.com>
2022-11-17 13:12:00 +0100

    adding 404 page
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h2>404</h2>
+      <p>Sorry, that page does't exists</p>
+    </div>
+  )
+}
+
+export default NotFound
