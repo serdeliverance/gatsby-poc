c7fb48020ecbf4a49c9f5a4f7f45f5f3af19b5d4
Sergio Cano <canosergio90@gmail.com>
2022-11-18 02:21:08 +0100

    adding css modules
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
+import * as styles from '../styles/home.module.css'
+import { Link } from 'gatsby'
 
 const Home = () => {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>Fullstack developer</p>
+          <Link className={styles.btn} to="/projects">
+            My Portfolio Projects
+          </Link>
         </div>
       </section>
     </Layout>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import * as styles from '../../styles/projects.module.css'
 
 const Projects = () => {
   return (
     <Layout>
-      <div>
+      <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Develop & Deploy</h3>
       </div>
