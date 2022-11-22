5902f3382373fad944f2bdd49acfa250f2a287bb
Sergio Cano <canosergio90@gmail.com>
2022-11-22 22:08:26 +0100

    page queries
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
 import * as styles from '../styles/home.module.css'
-import { Link } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 
-const Home = () => {
+const Home = ({ data }) => {
+  const { title, description } = data.site.siteMetadata
+
   return (
     <Layout>
       <section className={styles.header}>
@@ -16,9 +18,23 @@ const Home = () => {
           </Link>
         </div>
         <img src="/banner.png" alt="site banner" style={{ maxWidth: '100%' }} />
+        <p>
+          {title} - {description}
+        </p>
       </section>
     </Layout>
   )
 }
 
 export default Home
+
+export const query = graphql`
+  query SiteInfo {
+    site {
+      siteMetadata {
+        description
+        title
+      }
+    }
+  }
+`
